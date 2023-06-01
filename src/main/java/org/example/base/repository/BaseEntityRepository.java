6284f05ed0cb1d556793641faeb35077f8f869e9
Ali Bondar <bondarali1380@gmail.com>
2023-06-01 22:53:19 +0330

    base class and repo are added

---
+++ src/main/java/org/example/base/repository/BaseEntityRepository.java
@@ -0,0 +1,21 @@
+package org.example.base.repository;
+
+import org.example.base.domain.BaseEntity;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface BaseEntityRepository<T extends BaseEntity<ID>, ID extends Serializable> {
+
+    void save(T t);
+
+    void delete(T t);
+
+    void deleteById(ID id);
+
+    T findById(ID id);
+
+    List<T> findAll();
+
+    void update(T t);
+}
